134626930b75157eb795dd25bb5b06fbbae0110d
TheComet <alex.murray@gmx.ch>
2017-07-01 20:19:02 +0200

    Logarithmic magnitude in 3D plot

--- tfp/src/views/ComplexPlane3D.cpp
+++ tfp/src/views/ComplexPlane3D.cpp
@@ -4,8 +4,53 @@
 #include <QVBoxLayout>
 #include <qwt3d_surfaceplot.h>
 #include <qwt3d_function.h>
+#include <qwt3d_color.h>
 
 namespace tfp {
+
+class Jet : public Qwt3D::StandardColor
+{
+public:
+    explicit Jet(Qwt3D::Plot3D* data, unsigned size = 100) : Qwt3D::StandardColor(data, size) {}
+    void reset(unsigned size)
+    {
+        colors_ = Qwt3D::ColorVector(size);
+        Qwt3D::RGBA elem;
+     
+        double dsize = size;
+     
+        for (unsigned int i=0; i!=size; ++i)
+        {
+            elem.r = red(i/dsize);
+            elem.g = green(i/dsize);
+            elem.b = blue(i/dsize);
+            elem.a = 1.0;
+            colors_[i] = elem;
+        }
+    }
+
+    double interpolate( double val, double y0, double x0, double y1, double x1 ) {
+        return (val-x0)*(y1-y0)/(x1-x0) + y0;
+    }
+
+    double base( double val ) {
+        if ( val <= -0.75 ) return 0;
+        else if ( val <= -0.25 ) return interpolate( val, 0.0, -0.75, 1.0, -0.25 );
+        else if ( val <= 0.25 ) return 1.0;
+        else if ( val <= 0.75 ) return interpolate( val, 1.0, 0.25, 0.0, 0.75 );
+        else return 0.0;
+    }
+
+    double red( double gray ) {
+        return base( gray - 0.5 );
+    }
+    double green( double gray ) {
+        return base( gray );
+    }
+    double blue( double gray ) {
+        return base( gray + 0.5 );
+    }
+};
 
 class MagnitudeFunction : public Qwt3D::Function
 {
@@ -18,7 +63,7 @@ public:
 
     double operator()(double x, double y)
     {
-        return std::abs(tf_->evaluate(typename Type<double>::Complex(x, y)));
+        return std::log10(20 * std::abs(tf_->evaluate(typename Type<double>::Complex(x, y))));
     }
 
 private:
@@ -56,7 +101,8 @@ ComplexPlane3D::ComplexPlane3D(QWidget* parent) :
     plot_->setScale(1, 1, 1);
     plot_->setShift(0.15, 0, 0);
     plot_->setZoom(0.9);
-
+    plot_->setPlotStyle(Qwt3D::FILLED);
+    //plot_->setDataColor(new Jet(plot_));
     plot_->setCoordinateStyle(Qwt3D::BOX);
     plot_->setOrtho(false);
 
@@ -81,9 +127,9 @@ void ComplexPlane3D::onSystemParametersChanged()
 {
     MagnitudeFunction func(plot_, system_);
     
-    func.setMesh(80, 80);
+    func.setMesh(100, 100);
     func.setDomain(-5.5, 0.5, -3, 3);
-    func.setMinZ(0);
+    func.setMinZ(-5);
     func.setMaxZ(5);
     func.create();
 
@@ -95,9 +141,9 @@ void ComplexPlane3D::onSystemStructureChanged()
 {
     MagnitudeFunction func(plot_, system_);
 
-    func.setMesh(80, 80);
+    func.setMesh(100, 100);
     func.setDomain(-5.5, 0.5, -3, 3);
-    func.setMinZ(0);
+    func.setMinZ(-5);
     func.setMaxZ(5);
     func.create();
 }
